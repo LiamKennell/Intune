883805518ecba7d76b4c072384049ff5d51fbbeb
ColosseumLiam <123189014+LiamKennell@users.noreply.github.com>
2025-03-10 12:06:45 +0000

    Update RemoveAppsWin11-24H2.ps1
    Corrected filepath to RAW text file

--- RemoveAppsWin11-24H2.ps1
+++ RemoveAppsWin11-24H2.ps1
@@ -1,4 +1,4 @@
-$filePath = "https://github.com/LiamKennell/Intune/blob/main/AppList.txt"
+$filePath = "https://raw.githubusercontent.com/LiamKennell/Intune/refs/heads/main/AppList.txt"
 Get-Content -path $filePath | ForEach-Object {
     get-appxpackage -allusers $_ | remove-appxpackage -AllUsers -ErrorAction continue
 }
